26fb9f26a7ebfdd62fea3dffb78a19364369dc71
Prem Kumar <prem8309090125@gmail.com>
2024-09-18 00:03:37 +0530

    Update array_problems.js

--- js_dsa/array_problems.js
+++ js_dsa/array_problems.js
@@ -97,6 +97,92 @@ const findSecondSmallElement = (arr) => {
     
 }
 
+
+// Question type - 2 Rotating array by k
+
+// Approach 1 : Slice the array and re shuffle the array parts.
+// Approach 2 : Reverse the array and reverse the portions
+
+// rotate right by 3
+// 1 2 3 4 5 
+// rotate 1 = 5 1 2 3 4
+// rotate 2 = 4 5 1 2 3
+// rotate 3 = 3 4 5 1 2
+
+// rorate left by 3
+// 1 2 3 4 5
+// rotate 1 = 2 3 4 5 1
+// rotate 2 = 3 4 5 1 2
+// rotate 3 = 4 5 1 2 3
+
+// Reverse approach k = 3 for Left
+// 1 2 3 4 5 -> 5 4 3 2 1 -> 3 4 5   1 2
+
+// Reverse approach k = 2(5 - 3) for Right
+// 1 2 3 4 5 -> 5 4 3 2 1 -> 4 5   1 2 3
+
+
+const arr = [1,2,3,4,5];
+
+const reverseArray = (arr,start,end) => {
+    let i = start;
+    let j = end;
+    while(start<end){
+        let temp = arr[start]
+        arr[start++] = arr[end]
+        arr[end--] = temp
+    }
+}
+
+
+const rotateArrayRightByK = (arr,k)=>{
+    const size = arr.length
+    // if k greater than size then modulous
+    if(k > size){
+        k = k % size
+    }
+    reverseArray(arr,0,size - 1);
+    reverseArray(arr,0,k-1);
+    reverseArray(arr,k,size - 1);
+    return arr;
+}
+
+const rotateArrayLeftByK = (arr,k)=>{
+    return rotateArrayRightByK(arr,arr.length-k)
+}
+
+console.log(rotateArrayRightByK([1,2,3,4,5],3))
+console.log(rotateArrayLeftByK([1,2,3,4,5],3))
+
+
+// General Question type Swapping
+
+const swap = (a,b) => {
+    const temp = a;
+    a = b;
+    b = temp
+    console.log(a,b)
+}
+
+const swapWithOutUsingThird = (a,b)=>{
+    a = a + b;
+    b = a - b;
+    a = a - b;
+    
+    console.log(a,b)
+}
+
+
+
+
+
+
+
+
+
+
+
+
 console.log(findSecondSmallElement(arr))
 // For Nth smallest need to use sorting or heaps.
 
